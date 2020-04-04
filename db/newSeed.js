b4bd493bd9286f37027856d56a1690a9a92f2d97
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-04-03 17:37:57 -0700

    data is properly seeding and have successfully inserted 1000 items in my database. Next I will try to generate and insert 10mil records

--- db/newSeed.js
+++ db/newSeed.js
@@ -1,57 +1,58 @@
 const faker = require('faker');
 const fs = require('fs')
 
-let photoSet = 0;
+
 let indexer = 0;
 function photoDistubutor() {
-    if (indexer < 1000 && photoSet < 100) { 
-            let imageUrls = [];    
-            for (let i = 0; i < 10; i += 1) {
-                imageUrls.push(`https://steam-content-display.s3-us-west-2.amazonaws.com/steam_content${indexer}.jpg`);
-                indexer += 1; // when index hit 1000 indexer will reset
-            }
-
-            return imageUrls;
-        }else if(indexer === 1000){
+    let imageUrls = [];
+    for (let i = 0; i < 10; i += 1) {
+        imageUrls.push(`"https://steam-content-display.s3-us-west-2.amazonaws.com/steam_content${indexer}.jpg"`);
+        indexer += 1; // when index hit 1000 indexer will reset
+        if (indexer === 1000) {
             indexer = 0;
-             photoSet += 1;
         }
-    }
+    } return imageUrls;
+}
+
 
 
 //give every entry 3 videos and a thumbnail image of video
 function getVideos() {
-    videoArray = [];
-
+    let videoArray = [];
     for (let mediaIndexer = 0; mediaIndexer < 3; mediaIndexer += 1) {
         //screenShot[0] === thumbnail
         //screenShot[1] === videoUrl
-        const screenShot = `https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png`                      
-        const videoUrl = `https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4` + ',' 
-                            
-        videoArray.push(screenShot+ ',' + videoUrl)
-    }
-    return videoArray;
+        const screenShot = {
+            thumbnail: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png'`,
+            videoUrl: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4'`
+        }
+
+        videoArray.push(screenShot)
+    } console.log(JSON.stringify(videoArray))
+    return JSON.stringify(videoArray);
 }
+let stringifyVideoArray = getVideos();
 
 const entireGameDump = fs.createWriteStream('./game-reviews.csv', { flags: 'a' });
 for (let i = 0; i < 1000; i += 1) {
     entireGameDump.write(
-/*name */          faker.commerce.productName() + ',' +
-/*description */   faker.lorem.paragraph() + ',' +
-/*head_url */      photoDistubutor() + ',' +
-/*release_date */  faker.date.past() + ',' +
-/*developer */     faker.company.companyName() + ',' +
-/*publisher */     faker.company.companyName() + ',' +
-/*negative */      faker.random.number(1000) + ',' +
-/*positive */      faker.random.number(1000) + ',' +
-/*recent */        faker.random.number(100) + ',' +
-/*recent */        faker.random.number(100) + ',' +
-/*media_video */   getVideos() + '\n'
+/*name */          faker.commerce.productName() + '|' +
+/*description */   faker.lorem.paragraph() + '|' +
+/*head_url */      `[${photoDistubutor()}]` + '|' +
+/*release_date */  faker.date.past() + '|' +
+/*developer */     faker.company.companyName() + '|' +
+/*publisher */     faker.company.companyName() + '|' +
+/*negative */      faker.random.number(1000) + '|' +
+/*positive */      faker.random.number(1000) + '|' +
+/*recent */        faker.random.number(100) + '|' +
+/*recent */        faker.random.number(100) + '|' +
+/*media_video */   `${stringifyVideoArray}` + '\n' 
     );
 }
 entireGameDump.end();
 entireGameDump.on('finish', () => {
-    console.log('Files have been loaded')})
-    entireGameDump.on('error', () => {
-        console.error('******Please fix me, I am broken.********')})
+    console.log('Files have been loaded')
+})
+entireGameDump.on('error', () => {
+    console.error('******Please fix me, I am broken.********')
+})
--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,15 @@
+DROP TABLE IF EXISTS games ;
 
 CREATE TABLE games (
-gameid                      int,
 name                    varchar(50),
-description             varchar(140),
-url                     varchar(1100),
-developer               varchar(25),
-publisher               varchar(25),
+description             text,
+head_url                jsonb,
+release_date            varchar(70),
+developer               varchar(70),
+publisher               varchar(70),
 negative_review_count   int,
 positive_review_count   int,
 recent_negative_count   int,
 recent_positive_count   int,
-media_video             text[],
-)
+media_video             jsonb
+);
