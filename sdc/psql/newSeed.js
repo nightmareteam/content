5327e74dda5d86f5db553cbd92df193fb237da6b
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-04-03 23:57:55 -0700

    Code clean-up

---
+++ sdc/psql/newSeed.js
@@ -0,0 +1,74 @@
+const faker = require('faker');
+const fs = require('fs')
+
+
+let indexer = 0;
+function photoDistubutor() {
+    let imageUrls = [];
+    for (let i = 0; i < 10; i += 1) {
+        imageUrls.push(`"https://steam-content-display.s3-us-west-2.amazonaws.com/steam_content${indexer}.jpg"`);
+        indexer += 1; // when index hit 1000 indexer will reset
+        if (indexer === 1000) {
+            indexer = 0;
+        }
+    } return imageUrls;
+}
+
+
+
+//give every entry 3 videos and a thumbnail image of video
+function getVideos() {
+    let videoArray = [];
+    for (let mediaIndexer = 0; mediaIndexer < 3; mediaIndexer += 1) {
+        //screenShot[0] === thumbnail
+        //screenShot[1] === videoUrl
+        const screenShot = {
+            thumbnail: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png'`,
+            videoUrl: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4'`
+        }
+
+        videoArray.push(screenShot)
+    }
+    return JSON.stringify(videoArray);
+}
+let stringifyVideoArray = getVideos();
+
+
+
+const write = (writer, data) => {
+    if (!writer.write(data)) {
+        return new Promise((resolve) => {
+            writer.once('drain', resolve)
+        })
+    }
+}
+const gameGenerator = async (limit) => {
+    const writeEntireGameDump = fs.createWriteStream('./game-reviews-TEST.csv', { flags: 'a' });
+    let gameId = 1;
+    while (gameId <= limit) {
+        let singleGameData =
+                gameId + '|' +                        
+                faker.commerce.productName() + '|' +  
+                faker.lorem.paragraph() + '|' +       
+                `[${photoDistubutor()}]` + '|' +      
+                faker.date.past() + '|' +             
+                faker.company.companyName() + '|' +     
+                faker.company.companyName() + '|' +    
+                faker.random.number(1000) + '|' + 
+                faker.random.number(1000) + '|' + 
+                faker.random.number(100) + '|' +  
+                faker.random.number(100) + '|' +  
+                `${stringifyVideoArray}` + '\n'; 
+                  console.log(singleGameData)
+        let promise = write(writeEntireGameDump, singleGameData);
+        gameId++;
+        // since drain happens rarely, awaiting each write call is really slow.
+        if (promise) {
+            // we got a drain event, therefore we wait
+            await promise;
+        }
+        
+    }
+}
+gameGenerator(5);
+
---
+++ sdc/psql/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS games ;
+
+CREATE TABLE games (
+gameid                  int,
+name                    varchar(50),
+description             text,
+head_url                jsonb,
+release_date            varchar(70),
+developer               varchar(70),
+publisher               varchar(70),
+negative_review_count   int,
+positive_review_count   int,
+recent_negative_count   int,
+recent_positive_count   int,
+media_video             jsonb
+);
