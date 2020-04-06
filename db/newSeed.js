cbe6b0708de49e6f52e31fb39ada99b9346b31e6
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-04-06 15:36:07 -0700

    Brought over my correct seed data function

--- db/newSeed.js
+++ db/newSeed.js
@@ -1,75 +1,73 @@
-
-//TODO: make 10,000,000 unique game ids
-/**
- * each photo will have 
- */
 const faker = require('faker');
+const fs = require('fs')
 
-let game = [];
-let photoSet = 0;  let indexer = 0;
+
+let indexer = 0;
 function photoDistubutor() {
-   
-    if (photoSet < 10) {
-                                         //for s3 bucket indexer
-        while (indexer < 1000) {
-            let imageUrls = [];                                  // am sharing 1000 urls among 10Mil entries
-            for (let i = 0; i < 10; i += 1) {
-                imageUrls.push(`https://steam-content-display.s3-us-west-2.amazonaws.com/steam_content${indexer}.jpg`);
-                indexer += 1; // adds 1 to the 1000 max photo urls I am using
-            }
-            return imageUrls;
+    let imageUrls = [];
+    for (let i = 0; i < 10; i += 1) {
+        imageUrls.push(`"https://steam-content-display.s3-us-west-2.amazonaws.com/steam_content${indexer}.jpg"`);
+        indexer += 1; // when index hit 1000 indexer will reset
+        if (indexer === 1000) {
+            indexer = 0;
         }
-        photoSet += 1;
-    }
+    } return imageUrls;
 }
-// let game = [];
-// let indexer = 0;  //for s3 bucket indexer
-// let photoSet = 0;
-// function photoDistubutor() {
-//     while (photoSet < 10000) {
-//         while (indexer < 1000) {
-//             let imageUrls = [];                       // am sharing 1000 urls among 10Mil entries
-//             for (let i = 0; i < 10; i += 1) {                          // creates 10 images
-//                 imageUrls.push(`https://steam-content-display.s3-us-west-2.amazonaws.com/steam_content${indexer}.jpg`);
-//                 indexer += 1; // adds 1 to the 1000 max photo urls I am using
-//             }
-//             return imageUrls;
-//         }
-//         photoSet += 1;
-//     }
-// }
+
+
 
 //give every entry 3 videos and a thumbnail image of video
 function getVideos() {
-    videoArray = [];
+    let videoArray = [];
     for (let mediaIndexer = 0; mediaIndexer < 3; mediaIndexer += 1) {
+        //screenShot[0] === thumbnail
+        //screenShot[1] === videoUrl
         const screenShot = {
-            image: `https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png`,
-            videoLink: `https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4`
+            thumbnail: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png'`,
+            videoUrl: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4'`
         }
+
         videoArray.push(screenShot)
     }
-    return videoArray;
+    return JSON.stringify(videoArray);
 }
+let stringifyVideoArray = getVideos();
 
 
-for (let i = 0; i < 10000; i += 1) {
-    game.push({
-        id: i,
-        name: faker.commerce.productName(),
-        description: faker.lorem.paragraph(),
-        head_url: photoDistubutor(),                                   // array of image urls
-        release_date: faker.date.past(),
-        developer: faker.company.companyName(),
-        publisher: faker.company.companyName(),
-        negative_review_count: faker.random.number(1000),
-        positive_review_count: faker.random.number(1000),
-        recent_negative_count: faker.random.number(100),
-        recent_positive_count: faker.random.number(100),
-        media_video: getVideos(),  //videos and screen shot
 
-    })
-
+const write = (writer, data) => {
+    if (!writer.write(data)) {
+        return new Promise((resolve) => {
+            writer.once('drain', resolve)
+        })
+    }
 }
-
-console.log(game);
+const gameGenerator = async () => {
+    const writeEntireGameDump = fs.createWriteStream('./game-reviews.csv', { flags: 'a' });
+    let gameId = 1;
+    while (gameId <= 5) {
+        let singleGameData =
+                gameId + '|' +                        
+                faker.commerce.productName() + '|' +  
+                faker.lorem.paragraph() + '|' +       
+                `[${photoDistubutor()}]` + '|' +      
+                faker.date.past() + '|' +             
+                faker.company.companyName() + '|' +     
+                faker.company.companyName() + '|' +    
+                faker.random.number(1000) + '|' + 
+                faker.random.number(1000) + '|' + 
+                faker.random.number(100) + '|' +  
+                faker.random.number(100) + '|' +  
+                `${stringifyVideoArray}` + '\n'; 
+                  
+        let promise = write(writeEntireGameDump, singleGameData);
+        gameId++;
+        // since drain happens rarely, awaiting each write call is really slow.
+        if (promise) {
+            // we got a drain event, therefore we wait
+            await promise;
+        }
+        
+    }
+}
+gameGenerator();
---
+++ sdc/cassdb/README.txt
@@ -0,0 +1,4 @@
+
+Used to import csv file to Cassandra
+
+$ java -jar csv-to-sstable.jar <keyspace> <absolute/path/to/schema.cql> <absolute/path/to/input.csv> <absolute/path/to/output/dir> [optional csv prefs]
