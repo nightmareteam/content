c2d21215c34de5103a2666b1d3437c07a0ffec60
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-04-07 15:27:10 -0700

    Completed seeding scripts for CassDb. Have to import csv file into docker so cassadra can load hethe 10000000 entries

--- db/newSeed.js
+++ db/newSeed.js
@@ -13,27 +13,51 @@ function photoDistubutor() {
         }
     } return imageUrls;
 }
-
-
-
-//give every entry 3 videos and a thumbnail image of video
+//Gives every entry 3 videos and a thumbnail image of video
 function getVideos() {
     let videoArray = [];
     for (let mediaIndexer = 0; mediaIndexer < 3; mediaIndexer += 1) {
-        //screenShot[0] === thumbnail
-        //screenShot[1] === videoUrl
-        const screenShot = {
-            thumbnail: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png'`,
-            videoUrl: `'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4'`
-        }
+        const screenShot = 
+            `$$thumbnail:'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_screenshot${mediaIndexer}.png', ` + 
+           `videoUrl: 'https://steam-content-videos.s3-us-west-2.amazonaws.com/steam_content_video${mediaIndexer}.mp4'$$`;
+        
 
         videoArray.push(screenShot)
     }
+    
     return JSON.stringify(videoArray);
 }
 let stringifyVideoArray = getVideos();
+let insertVideos = stringifyVideoArray.replace(/"/g,'')
 
 
+        // Creates 1000 developers and publishers
+        let devCounter = 0;
+        let pubCounter = 0;
+        let developers = []; 
+        let publishers = []; 
+        function makeDevelopersAndPublishers(){
+            for (let count = 0; count < 1000; count += 1) {
+                developers.push(faker.company.companyName());
+                publishers.push(faker.company.companyName());
+            }
+        }
+        makeDevelopersAndPublishers();
+                // insert functions will return one of one thousand 
+        function insertDeveloper() {
+            devCounter += 1;
+            if (devCounter === 1000) {
+                devCounter = 0;
+            }
+            return developers[devCounter];
+        }
+        function insertPublisher() {
+            pubCounter += 1;
+            if (pubCounter === 1000) {
+                pubCounter = 0;
+            }
+            return publishers[pubCounter];
+        }
 
 const write = (writer, data) => {
     if (!writer.write(data)) {
@@ -45,29 +69,26 @@ const write = (writer, data) => {
 const gameGenerator = async () => {
     const writeEntireGameDump = fs.createWriteStream('./sdc/cassdb/game-reviews.csv', { flags: 'a' });
     let gameId = 1;
-    while (gameId <= 5) {
+    while (gameId <= 10) {
         let singleGameData =
-                gameId + '|' +                        
-                `'${faker.commerce.productName()}'` + '|' +  
-                `'${faker.lorem.paragraph()}'` + '|' +       
-                `[${photoDistubutor()}]` + '|' +      
-                `'${faker.date.past()}'` + '|' +             
-                `'${faker.company.companyName()}'` + '|' +     
-                `'${faker.company.companyName()}'` + '|' +    
-                faker.random.number(1000) + '|' + 
-                faker.random.number(1000) + '|' + 
-                faker.random.number(100) + '|' +  
-                faker.random.number(100) + '|' +  
-                `${stringifyVideoArray}` + '\n'; 
+                gameId + ',' +                        
+                `'${faker.commerce.productName()}'` + ',' +  
+                `'${faker.lorem.paragraph()}'` + ',' +       
+                `[${photoDistubutor()}]` + ',' +      
+                `'${faker.date.past()}'` + ',' +             
+                `'${insertDeveloper()}'` + ',' +     
+                `'${insertPublisher()}'` + ',' +    
+                faker.random.number(1000) + ',' + 
+                faker.random.number(1000) + ',' + 
+                faker.random.number(100) + ',' +  
+                faker.random.number(100) + ',' +  
+                `${insertVideos}` + '\n'; 
                   
         let promise = write(writeEntireGameDump, singleGameData);
         gameId++;
-        // since drain happens rarely, awaiting each write call is really slow.
         if (promise) {
-            // we got a drain event, therefore we wait
             await promise;
         }
-        
     }
 }
 gameGenerator();
