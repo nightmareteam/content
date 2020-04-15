95e0f9a28f4fdebe9eb80198171e280b4fa2d3f1
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-04-14 22:59:40 -0700

    Added Loader.io to server endpoint. Going to deploy to ec2 server

--- server/app.js
+++
@@ -1,9 +0,0 @@
-// const pool = require('../db')
-
-// const retrieveGameContent = (gameid) => {
-// return pool.query()
-
-//     //    return pool.query('SELECT * FROM updates WHERE game_id = $1', [gameId])
-// }
-
-// module.exports = retrieveGameContent;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,28 @@
 require('newrelic');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 const db = require('../db');
 const port = 3002;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/:id([0-9]*)', express.static(path.resolve(__dirname, '..', 'public')));
 
 app.get('/content/:id', (req, res) => {
 const reqId = req.params.id;
       db.retrieveGameContent(reqId)
         .then((result)=>res.send(result.rows[0]))
         .catch(e=>console.log(e))
-
       })
+app.get('/api/bundle.js',(req, res)=>{
+      res.sendFile(path.resolve(__dirname, '..', 'public', 'bundle.js'));
+})
+app.get('/loaderio-1dac395a0c5229294c14de07d32c8c2f/',(req, res)=>{
+      res.sendFile(path.resolve(__dirname, '..', 'loaderio-1dac395a0c5229294c14de07d32c8c2f.txt'))
+})
 
-
-      
 app.listen(port, () => { console.log('Listening to ' + port + '!');
 });
